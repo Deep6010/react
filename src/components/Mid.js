1d4e5f1ceb2327d7c425464be252aa73df54ec28
Deep Raj <rajdeep6010@gmail.com>
2022-01-10 11:17:30 +0545

    header/footer/mid formate

---
+++ src/components/Mid.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Mid extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is mid</p>
+            </div>
+        )
+    }
+}
+
